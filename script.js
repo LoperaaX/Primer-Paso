4fc1c29e395d2f3bbfd27a5d6e4754c7f4cc333f
LoperaaX <fernandotobon85@gmail.com>
2023-05-23 19:31:09 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function showAlert() { alert('¡ALERTA MATEO ES UN FALSO HIJO DE PUTA!'); }
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
